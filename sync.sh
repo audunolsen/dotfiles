38b7c885c1041af9618df3b6968587d1b48c86e4
Audun Meek Olsen <audunmeekolsen@gmail.com>
2019-03-07 12:33:54 +0100

    Dynamic symlinking of dotfiles belonging to homedir

--- sync.sh
+++ sync.sh
@@ -1,2 +1,18 @@
-ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
-ln -sf $HOME/dotfiles/.nvimrc $HOME/.nvimrc
+#!/bin/zsh
+setopt extended_glob
+
+# files=(.zshrc .nvimrc .gitconfig .hyper.css .hyper.js)
+# 
+# for file in $files; do
+#     ln -sf $HOME/dotfiles/$file $HOME/$file
+# done
+# 
+for f in **/.*~.git/*~.git(DN); do
+     ln -sf $HOME/dotfiles/$f $HOME/$f:t 
+done
+
+#ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
+#ln -sf $HOME/dotfiles/.nvimrc $HOME/.nvimrc
+#ln -sf $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+#ln -sf $HOME/dotfiles/.hyper.css $HOME/.hyper.css
+#ln -sf $HOME/dotfiles/.hyper.js $HOME/.hyper.js
